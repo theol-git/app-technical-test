4a6a5c6736915a2e4762de03c19747fd5d503140
Theo <theo.lettermann@gmail.com>
2024-04-05 16:56:25 +0200

    fix: mypy type fix

--- backend/app/app/api/dependencies.py
+++ backend/app/app/api/dependencies.py
@@ -1,8 +1,10 @@
+from collections.abc import AsyncGenerator
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.session import sessionmanager
 
 
-async def get_db_session() -> AsyncSession:
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
     async with sessionmanager.session() as session:
         yield session
